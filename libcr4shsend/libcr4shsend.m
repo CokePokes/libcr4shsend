adb590a88d474c7bfcd0118cf2b63bdc8e28eab9
CokePokes <cokepokes@gmail.com>
2021-05-04 05:32:30 -0500

    removed UIKit and add runtime UIAlertController

--- libcr4shsend/libcr4shsend.m
+++ libcr4shsend/libcr4shsend.m
@@ -14,12 +14,21 @@
 #endif
 
 #import <Foundation/Foundation.h>
-#import "CaptainHook/CaptainHook.h"
+#include <CaptainHook/CaptainHook.h>
 #include <notify.h> // not required; for examples only
-#import "CPUIAlertController+Window.h"
 #include <dlfcn.h>
 
-extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
+#import <UIKit/UIKit.h>
+@interface CPCustomUIAlertController :  UIAlertController
+@property (nonatomic, strong) UIWindow *alertWindow;
+- (void)setAlertWindow:(UIWindow *)alertWindow;
+- (UIWindow *)alertWindow;
+- (void)show;
+- (void)show:(BOOL)animated;
+- (void)viewDidDisappear:(BOOL)animated;
+@end
+
+extern CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
 
 @interface libcr4shsend : NSObject
 
@@ -49,8 +58,9 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
         }
     }
     if (!hasRequestedForBundleId){
-        CPUIAlertController *alC = [CPUIAlertController alertControllerWithTitle:@"Auto Crash Reports" message:[NSString stringWithFormat:@"The tweak: \"%@\" has requested automatic crash reports. Would you like to enable automatic crash reporting for this tweak?", tweakName] preferredStyle:UIAlertControllerStyleAlert];
-        [alC addAction:[UIAlertAction actionWithTitle:@"Enable" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                
+        CPCustomUIAlertController *alC = [objc_getClass("CPCustomUIAlertController") alertControllerWithTitle:@"Auto Crash Reports" message:[NSString stringWithFormat:@"The tweak: \"%@\" has requested automatic crash reports. Would you like to enable automatic crash reporting for this tweak?\n\nReports will be sent via your primary email on this device so the developer can respond if they need more info.", tweakName] preferredStyle:UIAlertControllerStyleAlert];
+        [alC addAction:[objc_getClass("UIAlertAction") actionWithTitle:@"Enable" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             NSDictionary *dic = @{@"bundleId": bundleId,
                                   @"tweakName": tweakName,
                                   @"permissionGranted" : [NSNumber numberWithBool:YES] };
@@ -59,7 +69,7 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
             CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(), CFSTR("com.cokepokes.libcr4shsend-requestPermission"), (__bridge const void *)json, NULL, true);
             block(YES);
         }]];
-        [alC addAction:[UIAlertAction actionWithTitle:@"Reject" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        [alC addAction:[objc_getClass("UIAlertAction") actionWithTitle:@"Reject" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
             NSDictionary *dic = @{@"bundleId": bundleId,
                                   @"tweakName": tweakName,
                                   @"permissionGranted" : [NSNumber numberWithBool:NO] };
@@ -155,9 +165,44 @@ static void registerReports(CFNotificationCenterRef center, void *observer, CFSt
     [prefs.copy writeToFile:filePath atomically:YES];
 }
 
+
+CHDeclareClass(UIAlertController);
+CHDeclareClass(CPCustomUIAlertController);
+CHPropertyRetainNonatomic(CPCustomUIAlertController, UIWindow*, alertWindow, setAlertWindow);
+
+
+CHOptimizedMethod0(new, void, CPCustomUIAlertController, show){
+    [self show:YES];
+}
+
+CHOptimizedMethod1(new, void, CPCustomUIAlertController, show, BOOL, animated){
+    self.alertWindow = [[objc_getClass("UIWindow") alloc] initWithFrame:[NSClassFromString(@"UIScreen") mainScreen].bounds];
+    self.alertWindow.rootViewController = [[objc_getClass("UIViewController") alloc] init];
+    self.alertWindow.windowLevel = 1000 + 1;
+    [self.alertWindow makeKeyAndVisible];
+    [self.alertWindow.rootViewController presentViewController:(UIAlertController*)self animated:animated completion:nil];
+}
+
+CHMethod1(void, CPCustomUIAlertController, viewDidDisappear, BOOL, animated) {   
+    CHSuper1(CPCustomUIAlertController, viewDidDisappear, animated);
+    self.alertWindow.hidden = YES;
+    self.alertWindow = nil;
+}
+
+
 CHConstructor {
     @autoreleasepool {
+        
+        CHLoadLateClass(UIAlertController);
+        CHRegisterClass(CPCustomUIAlertController, UIAlertController) {
+            CHHookProperty(CPCustomUIAlertController, alertWindow, setAlertWindow);
+            CHHook0(CPCustomUIAlertController, show);
+            CHHook1(CPCustomUIAlertController, show);
+            CHHook1(CPCustomUIAlertController, viewDidDisappear);
+        }
+        
         if ([NSProcessInfo.processInfo.processName isEqualToString:@"SpringBoard"]){ //only want springboard to run server
+
             void *handle = dlopen("/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation", RTLD_LAZY);
             void *impl = NULL;
             if (handle) {
